7baca861961857078998b4447df648110fded703
Moulali <66048515+Shaik-Moulali@users.noreply.github.com>
2021-01-19 20:46:53 +0530

    Update Springboot1Application.java

--- src/main/java/com/moulali/spring/boot/springboot1/Springboot1Application.java
+++ src/main/java/com/moulali/spring/boot/springboot1/Springboot1Application.java
@@ -3,7 +3,8 @@ package com.moulali.spring.boot.springboot1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;  
-import org.springframework.web.bind.annotation.RestController;  
+import org.springframework.web.bind.annotation.RestController; 
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @RestController
